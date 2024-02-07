69dc40cdf0182075ecce29628b012d9118c77edb
Ali Amanzadegan <persiaprog@gmail.com>
2024-02-07 16:05:10 +0330

    Add PlayerState C++ base class and create a blueprint based on it and set it in aura game mode. just set NetUpdateFrequency to a bigger number

---
+++ Source/LearnGAS/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Ali Amanzadegan
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/LearnGAS/Public/Player/AuraPlayerState.h
@@ -0,0 +1,20 @@
+// Copyright Ali Amanzadegan
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNGAS_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	
+};
